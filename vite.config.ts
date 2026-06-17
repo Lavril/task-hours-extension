ad1870fc6339675ab247de216b250b55c13b8999
Vlasislav Sudakov <v_a_sudakov@inbox.ru>
2026-06-17 21:43:19 +0300

    Add GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { viteStaticCopy } from "vite-plugin-static-copy"
 
+const isGithubPages =
+  process.env.GITHUB_PAGES ===
+  "true"
+
 export default defineConfig({
+  base: isGithubPages
+    ? "/task-hours-extension/"
+    : "./",
+
   plugins: [
     react(),
 
